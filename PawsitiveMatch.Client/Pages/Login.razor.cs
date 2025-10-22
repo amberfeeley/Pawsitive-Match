2997ad5af20a721a122b5e49cc25b52ad3fb2340
o-licea <oraciolicea@outlook.com>
2025-10-21 20:16:19 -0500

    Adding files to reflect the Wireframe design

---
+++ PawsitiveMatch.Client/Pages/Login.razor.cs
@@ -0,0 +1,10 @@
+namespace PawsitiveMatch.Client.Pages
+{
+    // Partial class definition needed for the Login page to detect the code
+    public partial class Login
+    {
+        // Test functions
+        public string TextValue { get; set; } = string.Empty;
+        public string PasswordValue { get; set; } = string.Empty;
+    }
+};
