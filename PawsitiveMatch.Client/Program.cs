31452de09aa1d48ce23334cd0014d9500d8a4587
Amber Feeley <amberfeeley@yahoo.com>
2025-10-25 14:23:12 -0500

    28: https fixes

--- PawsitiveMatch.Client/Program.cs
+++ PawsitiveMatch.Client/Program.cs
@@ -6,7 +6,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Services.AddScoped(sp => new HttpClient
 {
-    BaseAddress = new Uri("https://localhost:5001/")
+    BaseAddress = new Uri("http://localhost:5004/")
 });
 
 builder.Services.AddScoped<ApiService>();
